11e44aa829e2f12831d7261ab655e8bd2b1548dc
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2025-07-31 20:07:34 +0300

    database integration

--- api/main.py
+++ api/main.py
@@ -1,37 +1,92 @@
-from fastapi import FastAPI
+from typing import Annotated
 
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
 
 app = FastAPI()
 
-# books = { "1" : ["Name : HarryPotter","Author : JK Rowling","Year : 1987"],
-#          "2" : ["Name : GameOfThrones","Author : George R.R Martin","Year : 1979"],
-#          "3" : ["Name : LordOfTheRings","Author : John R.R Tolkein","Year : 1956"]
-#           }
+# Creating a Model
 
-books = []
-
-class Book(BaseModel):
-    id : int
-    name : str
-    author : str
+class Book(SQLModel, table=True):
+    id : int | None = Field(default=None, primary_key=True)
+    name : str = Field(index=True)
+    author : str = Field(index=True)
     year : int
 
+# Creating an Engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+# Creating the Tables
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+# Creating a Session Dependecy
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+# Creating DataBase Tables at StartUp
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
 @app.post('/books/')
-async def createBooks(book : Book):
+async def createBooks(book : Book, session: SessionDep) -> Book:
 
-    books.append(book)
-
+    session.add(book)
+    session.commit()
+    session.refresh(book)
     return book
 
 
 @app.get('/books/')
-async def getBooks():
-
-    return(books)
+async def getBooks(
+    session : SessionDep,
+    offset : int = 0,
+    limit : Annotated[int, Query(le=100)] = 100,
+) -> list[Book]:
     
-@app.get("/books/{bookId}")
-async def getBookById(bookId : int):
+    books = session.exec(select(Book).offset(offset).limit(limit)).all()
 
-    return (books[bookId])
+    return books
+    
+@app.get("/books/{book_id}")
+async def getBookById(book_id : int, session : SessionDep) -> Book:
 
+    book = session.get(Book, book_id)
+
+    if not book:
+        raise HTTPException(status_code=404, detail='Book not Found!')
+
+    return book
+
+@app.delete("/book/{book_id}")
+async def deleteBook(book_id :int, session:SessionDep) -> Book:
+
+    book = session.get(Book, book_id)
+
+    if not book:
+        raise HTTPException(status_code=404, detail='Book not Found!')
+    
+    session.delete(book)
+    session.commit()
+
+    return {"ok", True}
